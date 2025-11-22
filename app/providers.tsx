00e97b65c1325ba7c916a84a6a980c31b3bd44e3
bishal <bishalpd@gmail.com>
2025-11-22 16:11:44 +0600

    clerk added

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,9 +3,11 @@ import { ThemeProvider } from "./theme-provider";
 
 function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-      {children}
-    </ThemeProvider>
+    <>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        {children}
+      </ThemeProvider>
+    </>
   );
 }
 export default Providers;
