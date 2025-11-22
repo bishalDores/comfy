3a1cb624eb154382bf4f143f162e0e22f0771813
bishal <bishalpd@gmail.com>
2025-11-22 16:26:01 +0600

    middleware updated

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher(["/", "/products(.*)", "/about"]);
 
 export default clerkMiddleware((auth, req) => {
+  const { userId } = auth();
   if (!isPublicRoute(req)) auth().protect();
 });
 
